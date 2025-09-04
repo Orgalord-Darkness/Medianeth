bc7ae450d1e8dfd2f6513f6bc02c982ce9deb2db
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-09-04 16:19:17 +0200

    Création classe Movie

---
+++ models/Movie.php
@@ -0,0 +1,65 @@
+<?php
+
+    enum Genre{
+        case Action ; 
+
+        case Aventure ; 
+
+        case Comédie ; 
+
+        case Drame ; 
+
+        case Horreur ; 
+
+        case Thriller ; 
+
+        case Fantastique ;
+
+        case ScienceFiction ; 
+
+        case Fantasy ; 
+
+        case Policier ; 
+
+        case Romance ; 
+
+        case Guerre ; 
+
+        case Western ; 
+
+        case Animation ; 
+
+        case Documentaire ; 
+
+        case Biopic ; 
+
+        case Historique ; 
+    }
+    class Movie extends Media{
+
+        private float $duration; 
+        private Genre $genre; 
+
+        public function __construct($duration, $genre){
+            $this->duration = $duration; 
+            $this->genre = $genre; 
+        }
+
+        public function getDuration(){
+            return $this->duration; 
+        }
+
+        public function setDuration(float $duration){
+            $this->duration = $duration; 
+        }
+
+        public function getGenre(){
+            return $this->genre; 
+        }
+
+        public function setGenre(Genre $genre){
+            $this->genre = $genre; 
+        }
+
+
+    }
